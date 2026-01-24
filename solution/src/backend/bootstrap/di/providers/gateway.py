c5f9cadaeff538056ace6c5d8a10513d3bdd8114
RigalHD <profoundromeo@gmail.com>
2026-01-24 23:59:19 +0300

    app launch fix

--- solution/src/backend/bootstrap/di/providers/gateway.py
+++ solution/src/backend/bootstrap/di/providers/gateway.py
@@ -1,8 +1,7 @@
 from dishka import Provider, Scope, WithParents, provide_all
 
-from backend.application.common.gateway.fraud_rule import FraudRuleEvaluationResultGateway
-from backend.application.common.gateway.transaction import TransactionGateway
-from backend.infrastructure.database.gateway.fraud_rule import SAFraudRuleGateway
+from backend.infrastructure.database.gateway.fraud_rule import SAFraudRuleEvaluationResultGateway, SAFraudRuleGateway
+from backend.infrastructure.database.gateway.transaction import SATransactionGateway
 from backend.infrastructure.database.gateway.user import SAUserGateway
 
 
@@ -12,6 +11,6 @@ class GatewayProvider(Provider):
     gateways = provide_all(
         WithParents[SAUserGateway],
         WithParents[SAFraudRuleGateway],
-        WithParents[FraudRuleEvaluationResultGateway],
-        WithParents[TransactionGateway],
+        WithParents[SAFraudRuleEvaluationResultGateway],
+        WithParents[SATransactionGateway],
     )
